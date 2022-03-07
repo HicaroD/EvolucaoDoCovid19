311262898b3cc08c19ffd53ea48e49d226b66756
HicaroD <hdanrlley1@gmail.com>
2022-03-07 19:29:00 -0300

    Obtendo cada coluna (`date`, `World`) separadamente
    Tomei a decisão de lidar com as duas de forma separada, para isso tive
    que ler o dataset em `__init__` para não precisar ficar revisitando o
    dataset todas as vezes que eu quiser ler os dados

--- src/main.py
+++ src/main.py
@@ -5,14 +5,17 @@ COVID_DATASET_PATH = "datasets/total_cases.csv"
 class Forecaster:
     def __init__(self, n):
         self.number_of_days = n
+        self.covid_dataset = pd.read_csv(COVID_DATASET_PATH, usecols=['date', 'World'])
 
-    def fetch_total_cases_from_csv(self):
-        covid_dataset = pd.read_csv(COVID_DATASET_PATH, usecols=['date', 'World'])
-        return covid_dataset
+    def fetch_number_of_cases_column(self):
+        return self.covid_dataset.iloc[:, 1]
+
+    def fetch_date_column(self):
+        return self.covid_dataset.iloc[:, :-1]
 
     def predict(self):
-        total_cases = self.fetch_total_cases_from_csv()
-        print(total_cases)
+        date, number_of_cases = self.fetch_date_column(), self.fetch_number_of_cases_column()
+        print(date, number_of_cases)
 
 def main():
     n = int(input("Insira o número de dias: "))
