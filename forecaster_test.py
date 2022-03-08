50e181eab1f140371e9fa9bd743fb9d10e7fe70a
HicaroD <hdanrlley1@gmail.com>
2022-03-08 11:10:16 -0300

    Adicionando teste unitário para valor `const`

--- forecaster_test.py
+++ forecaster_test.py
@@ -1,8 +1,16 @@
 import unittest
+import forecaster
+import pandas as pd
 
 class TestLinearRegressionModel(unittest.TestCase):
-    pass
+    def test_const_value(self):
+        forecaster_ = forecaster.Forecaster("datasets/dummy_dataset.csv")
+        number_of_cases = forecaster_.fetch_number_of_cases_column()
 
-class TestForecaster(unittest.TestCase):
-    pass
+        linear_regression_model = forecaster.LinearRegressionModel()
+        self.const, _ = linear_regression_model.get_formula_data(number_of_cases)
 
+        self.assertEqual(round(self.const, 1), 2.2)
+
+if __name__ == "__main__":
+    unittest.main()
